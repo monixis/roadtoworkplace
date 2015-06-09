75b16a5bb3807c58a344f6aa1b2d72f2cf062d91
monixis <snh.monish@gmail.com>
2015-06-09 10:56:50 -0400

    Admin page 2.0

--- application/controllers/rtw.php
+++ application/controllers/rtw.php
@@ -3,26 +3,25 @@ class rtw extends CI_Controller {
 	public function index() {
 		$this -> load -> model('rtw_model');
 		//$data['result'] = $this -> rtw_model -> getemployers();
-		$data['title'] = "Road to the Workplace";		
+		$data['title'] = "Road to the Workplace";
 		$data['emptype'] = $this -> rtw_model -> getemployerstype();
 		$data['majors'] = $this -> rtw_model -> getmajors();
 		$data['majors1'] = $this -> rtw_model -> getmajors1();
 		$data['industry'] = $this -> rtw_model -> getindustry();
 		$data['industry1'] = $this -> rtw_model -> getindustry1();
-		$this -> load -> view('rtw_view', $data); 
+		$this -> load -> view('rtw_view', $data);
 	}
-	
 
 	public function getemployerdetails() {
 		$this -> load -> model('rtw_model');
-		$eid = $this -> input -> get('eid');	
+		$eid = $this -> input -> get('eid');
 		$data['title'] = "Road to the Workplace";
-		$data['result'] = $this -> rtw_model -> getemployerdetails($eid);	
-		$data['majors'] = $this -> rtw_model -> getassociatedmajors($eid);	
-		$data['industries'] = $this -> rtw_model -> getassociatedindustry($eid);				
+		$data['result'] = $this -> rtw_model -> getemployerdetails($eid);
+		$data['majors'] = $this -> rtw_model -> getassociatedmajors($eid);
+		$data['industries'] = $this -> rtw_model -> getassociatedindustry($eid);
 		$this -> load -> view('employer_view', $data);
 	}
-	
+
 	public function getrefinedemployers() {
 		$this -> load -> model('rtw_model');
 		$qry = $this -> input -> get('qry');
@@ -31,54 +30,9 @@ class rtw extends CI_Controller {
 		//$data['emptype'] = $this -> rtw_model -> getemployerstype();
 		//$data['majors'] = $this -> rtw_model -> getmajors();
 		//$data['industry'] = $this -> rtw_model -> getindustry();
-		$this -> load -> view('main_view', $data);		
+		$this -> load -> view('main_view', $data);
 	}
-
-	public function getAdminPage(){
-		$this -> load -> model('rtw_model');
-		$data['allemployers'] = $this -> rtw_model -> getemployers();
-		$eid = $this -> input -> get('eid');
-		$data['specificemployer'] = $this -> rtw_model -> getspecificemployer($eid);
-		$this -> load -> view('admin_rtw', $data);
-	}
-
-	public function getResultsAdmin(){
-		$this -> load -> model('rtw_model');
-		$search = $this -> input -> get('qry');
-		$data['res'] = $this -> rtw_model -> getempresults($search);
-		// $eid = $this -> input -> get('eid');
-		// $data['specificemployer'] = $this -> rtw_model -> getspecificemployer($eid);
-		$data['allemployers'] = $this -> rtw_model -> getemployers();
-		$this -> load -> view('admin_rtw', $data);
-	}
-
-	public function dbResultToAdmin(){
-		$this -> load -> model('rtw_model');
-		if($this -> input -> post("type") == "Update"){
-			$emp = $this -> input -> post();
-			$emp['timestamp'] = "DATE_FORMAT(NOW(), '%m-%d-%y %T')";
-			$emp['region'] = implode(", ", $emp['region']);
-			$emp['industry'] = implode(", ", $emp['industry']);
-			$emp['majors'] = implode(", ", $emp['majors']);
-			// $data['success'] = $this -> rtw_model -> updateEmp($emp);
-		}
-		else if($this -> input -> post("type") == "Delete"){
-			$eid = $this -> input -> post("eid");
-			$data['success'] = $this -> rtw_model -> deleteEmp($eid);
-		}
-		else if($this -> input -> post("type") == "Add"){
-			$emp = $this -> input -> post();
-			$emp['timestamp'] = "DATE_FORMAT(NOW(), '%m-%d-%y %T')";
-			$emp['region'] = implode(", ", $emp['region']);
-			$emp['industry'] = implode(", ", $emp['industry']);
-			$emp['majors'] = implode(", ", $emp['majors']);
-			$data['success'] = $this -> rtw_model -> addEmp($emp);
-		}
-		$data['success'] = $this -> input -> post();
-		$data['allemployers'] = $this -> rtw_model -> getemployers();
-		$this -> load -> view('admin_rtw', $data);
-	}
-		
+	
 	public function getemployers(){
 		$this -> load -> model('rtw_model');
 		$data['result'] = $this -> rtw_model -> getemployers();
@@ -88,5 +42,58 @@ class rtw extends CI_Controller {
 	public function disclaimer(){
 		$this -> load -> view('disclaimer');	
 	}	
+
+	/* For Admin page */
+
+	public function admin() {
+		$this -> load -> model('rtw_model');
+		$data['result'] = $this -> rtw_model -> getemployers();
+		$this -> load -> view('admin', $data);
+	}
+
+	public function getemployerdetailsforAdmin() {
+		$this -> load -> model('rtw_model');
+		$eid = $this -> input -> get('eid');
+		$data['title'] = "Admin - Road to the Workplace";
+		$data['result'] = $this -> rtw_model -> getemployerdetails($eid);
+		$data['majors'] = $this -> rtw_model -> getassociatedmajors($eid);
+		$data['industries'] = $this -> rtw_model -> getassociatedindustry($eid);
+		$this -> load -> view('admin_view', $data);
+	}
+
+	public function form_show() {
+		$this -> load -> view("admin_view.php");
+	}
+	
+	public function data_submitted(){
+		$col = $_POST['col'];
+		$val = $_POST['val'];
+		$eid = $_POST['eid'];
+		$this -> load -> model('rtw_model');
+		$result = $this -> rtw_model -> updaterecords($eid, $col, $val);
+		echo $result;
+		
+	}
+
+	public function newemployer() {
+		$this -> load -> model('rtw_model');
+		$data['emptype'] = $this -> rtw_model -> getemployerstype();
+		$data['majors'] = $this -> rtw_model -> getmajors();
+		$data['majors1'] = $this -> rtw_model -> getmajors1();
+		$data['industry'] = $this -> rtw_model -> getindustry();
+		$data['industry1'] = $this -> rtw_model -> getindustry1();
+		$this -> load -> view('newemployer', $data);
+	}
+	
+	public function newentry(){
+		$col = $_POST['col'];
+		$val = $_POST['val'];
+		$eid = $_POST['eid'];
+		$this -> load -> model('rtw_model');
+		$result = $this -> rtw_model -> updaterecords($eid, $col, $val);
+		echo $result;
+		
+	}
+	
 }
 ?>
--- application/models/rtw_model.php
+++ application/models/rtw_model.php
@@ -8,7 +8,7 @@ class rtw_model extends CI_Model {
 	}
 
 	function getemployerdetails($eid) {
-		$sql = "SELECT empname, website, corporatewebsite, additionalwebsites, leadership, hrcontactinfo, jobfaircontactinfo, location, region, geo, noofemp, emptype, ticker, affiliates, news, budget, facebook, twitter, socialmedia, missionstmt, overview, culture, financials, citations, poi, size, linkedin, taxforms from employer where eid = $eid";
+		$sql = "SELECT eid, empname, website, corporatewebsite, additionalwebsites, leadership, hrcontactinfo, jobfaircontactinfo, location, region, geo, noofemp, emptype, ticker, affiliates, news, budget, facebook, twitter, socialmedia, missionstmt, overview, culture, financials, citations, poi, size, linkedin, taxforms from employer where eid = $eid";
 		$results = $this -> db -> query($sql, array($eid));
 		return $results -> result();
 	}
@@ -43,38 +43,6 @@ class rtw_model extends CI_Model {
 		return $results -> result();
 	}
 
-	function deleteEmp($eid){
-		if ($this -> db -> simple_query("DELETE FROM employer WHERE eid = $eid")) {
-			$this -> db -> query("DELETE FROM employer WHERE eid = $eid");
-			return "Delete Successful";
-		} 
-		else {
-			return "Delete Unsuccessful";
-		}
-	}
-
-	function updateEmp($emp){
-		$this -> db -> where('eid' , $emp['eid']);
-		$this -> db -> update("employer", $emp);
-	}
-
-	function addEmp($emp){
-		// $sql = "INSERT INTO employer VALUES ('" . $emp['timestamp'] . "', " . $emp['eid'] . 
-		// 	", '" . $emp['empname'] . "', '" . $emp['missionstmt'] . "', '" . $emp['overview'] . 
-		// 	"', '" . $emp['website'] . "', '" . $emp['corporatewebsite'] . "', '" . $emp['additionalwebsites'] . 
-		// 	"', '" . $emp['leadership'] . "', '" . $emp['culture'] . "', '" . $emp['hrcontactinfo'] . 
-		// 	"', '" . $emp['jobfaircontactinfo'] . "', '" . $emp['location'] . "', '" . $emp['region'] . 
-		// 	"', '" . $emp['geo'] . "', '" . $emp['size'] . "', '" . $emp['noofemp'] . "', '" . $emp['emptype'] . 
-		// 	"', '" . $emp['ticker'] . "', '" . $emp['affiliates'] . "', '" . $emp['financials'] . "', '" . $emp['industry'] .
-		// 	 "', '" . $emp['majors'] . "', '" . $emp['news'] . "', '" . $emp['citations'] . "', '" . 
-		// 	 "', '" . $emp['yourName'] . "', '" . $emp['budget'] . "', '" . $emp['facebook'] . "', '" . $emp['twitter'] . 
-		// 	 "', '" . $emp['socialmedia'] . "', '" . $emp['taxforms'] . "', '" . $emp['linkedin'] . "', '" . $emp['poi'] . "')" ;
-
-		// $query = $this -> db -> query($sql);
-		$this -> db -> insert('employer', $emp);
-		return $emp['empname'] . " Added";
-	}
-	
 	function getemployerstype() {
 		$sql = "SELECT tid, type from employertype ORDER BY type ASC";
 		$results = $this -> db -> query($sql);
@@ -100,8 +68,7 @@ class rtw_model extends CI_Model {
 		return $results -> result();
 	}
 	
-	
-	function getindustry1() {
+		function getindustry1() {
 		$sql = "SELECT iid, industry from industry industry WHERE iid > 10";
 		$results = $this -> db -> query($sql);
 		return $results -> result();
@@ -112,6 +79,19 @@ class rtw_model extends CI_Model {
 		$sql = $sql . $qry;
 		$results = $this -> db -> query($sql);
 		return $results -> result();
+	}
+	
+	function updaterecords($eid,$col,$val){
+		$sql ="UPDATE employer SET $col = '$val' WHERE eid = $eid";
+		$result = $this -> db -> query($sql, array($col, $val, $eid));
+		return $result;
+		
+		/*$data = array(
+				$col => $val
+		);
+		
+		$this -> db -> where('eid', $eid);
+		$this -> db -> update('employer', $data);*/
 	}
 	
 }
